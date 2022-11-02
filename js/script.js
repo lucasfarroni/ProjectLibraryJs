b607ded2ca51a915cde3c0bef1aefa5f7428b801
Clement <clement.perrin9@etu.univ-lorraine.fr>
2022-11-02 09:00:10 +0100

    css flex sur card + margin

--- js/script.js
+++ js/script.js
@@ -1,30 +1,85 @@
+let i = 0;
 
-document.getElementById('btnAddMedia').addEventListener('click',function () {
+document.getElementById('btnAddMedia').addEventListener('click', function () {
 
-    document.getElementById('form').style.display="block";
+    document.getElementById('form').style.display = "block";
 
-    document.getElementById('btnSubmit').addEventListener('click',function () {
+    document.getElementById('btnSubmit').addEventListener('click', function () {
         // i++;
 
         console.log("test");
-        document.getElementById('form').style.display="none";
+        document.getElementById('form').style.display = "none";
         name = document.getElementById('title').value;
         document.getElementById('containerList').innerHTML +=
 
-            `<div class="card ` + i + ` " style="width: 18rem;">`+
+            `<div class="card" name ="` + name + `" style="width: 18rem;">` +
             '<img class="card-img-top" src="#" alt="Card image cap">' +
             '<div class="card-body">' +
             '<h5 class="card-title">' + name + '</h5>' +
             '<p class="card-text">Some quick example text to build on the card title and make up the bulk of thecards content.</p>' +
             '<a href="#" class="btn btn-primary">Go somewhere</a>' +
-            '</div>' + `<button type="button" id="remove` + i + `" class="btn btn-danger">delete</button>`+
+            '</div>' + `<button type="button" id="remove` + i + `" class="btn-delete">delete</button>` +
             '</div>';
     });
 
 });
 
-let i = 0;
+
+document.addEventListener("click", function (e) {
+    if (e.target.className === "btn-delete") {
+        if (confirm("Voulez vous vraiment supprimer ce média ?") == true) {
+            e.target.parentNode.remove();
+        }
+    }
+
+});
+
+document.addEventListener("click", function (e) {
+    if (e.target.id === "All") {
+        console.log("All");
+        const card = document.querySelectorAll('.card');
+        card.forEach(function (card) {
+            card.style.display = "block";
+        });
+    }
+    if (e.target.id === 'Album') {
+        console.log("Album");
+        const card = document.querySelectorAll('.card');
+        card.forEach(function (card) {
+            if (card.getAttribute('name') != 'Album') {
+                card.style.display = "none";
+            } else {
+                card.style.display = "block";
+            }
+        });
+    }
+    if (e.target.id === 'Game') {
+        const card = document.querySelectorAll('.card');
+        card.forEach(function (card) {
+            if (card.getAttribute('name') != 'Game') {
+                card.style.display = "none";
+            } else {
+                card.style.display = "block";
+            }
+        });
+
+    }
+    if (e.target.id === 'Movie') {
+        const card = document.querySelectorAll('.card');
+        card.forEach(function (card) {
+            if (card.getAttribute('name') != 'Movie') {
+                card.style.display = "none";
+            } else {
+                card.style.display = "block";
+            }
+        });
+
+
+    }
+});
+
+
 // Rating Initialization
-$(document).ready(function() {
+$(document).ready(function () {
     $('#rateMe2').mdbRate();
 });
