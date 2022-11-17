c90dc01af28499e71bb3963bb18e688a6038eb80
Farronilucas <farronilucas@hotmail.fr>
2022-11-17 15:26:14 +0100

    nettoyage

---
+++ Controller/Collection.js
@@ -0,0 +1,19 @@
+import {Media} from "./Media.js";
+
+export class Collection {
+
+    constructor() {
+        this.collection = [];
+    }
+
+    addMedia(obj) {
+
+        this.collection.push(obj);
+        localStorage.setItem("Collection", JSON.stringify(this.collection));
+
+    }
+
+    getCollection() {
+        return this.collection;
+    }
+}
