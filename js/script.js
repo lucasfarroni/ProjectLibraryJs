c304ba7a055faba0b7796acd75fe4fdb4b75115d
Clement <clement.perrin9@etu.univ-lorraine.fr>
2022-11-26 09:10:36 +0100

    PURGATION

--- js/script.js
+++ js/script.js
@@ -28,26 +28,6 @@ async function apiCall(movieTitle) {
         .catch(error => console.log(error));
 }
 
-/*
-function getTheTypeOfMedia(type){// fonction à refaire pour recup le type de media
-    switch (type.type) {
-
-        case "Album-btn":
-            console.log("album");
-            typeOfMedia = "Album";
-            break;
-        case "Game-btn":
-            console.log("Game");
-            typeOfMedia = "Game";
-            break;
-        case "Movie-btn":
-            console.log("livre");
-            typeOfMedia = "Movie";
-            break;
-    }
-    return typeOfMedia;
-}*/
-
 
 /**
  * @name addMedia
@@ -58,7 +38,7 @@ document.getElementById('btnAddMedia').addEventListener('click', function () {
         document.getElementById('form').style.display = "none";
     });
 
-    /*document.getElementById('type').addEventListener('click', function () {
+    document.getElementById('type').addEventListener('click', function () {
 
         switch (document.getElementById('type').value) {
 
@@ -76,13 +56,15 @@ document.getElementById('btnAddMedia').addEventListener('click', function () {
                 break;
         }
         return typeOfMedia;
-    });*/
+    });
 
     document.getElementById('form').style.display = "block";
 
+
     document.getElementById('btnSubmit').addEventListener('click', async function () {
         // i++;
-        let media = new Media(document.getElementById('title').value, document.getElementById('date').value, "rating", "img", document.getElementById('subject').value);
+
+        let media = new Media(document.getElementById('title').value, document.getElementById('date').value, "rating", "img", document.getElementById('subject').value, document.getElementById('type').value);
         document.getElementById('form').style.display = "none";
 
         console.log(localStorage.getItem('Collection') === null);
@@ -93,17 +75,16 @@ document.getElementById('btnAddMedia').addEventListener('click', function () {
             let similar = media.title;
             let index = tab.findIndex((media) => media.title === similar);
             console.log(index);
-
             if (index === -1) {
-                console.log("ok le titre pas dans la table");
+
+                console.log("ok ya r dans la tab");
                 const a = await apiCall(media.title);
                 console.log(a);
                 media.img = a.Poster;
-                console.log("un appel");
                 tab.push(media);
                 localStorage.setItem('Collection', JSON.stringify(tab));
 
-                document.getElementById('list').innerHTML +=
+                document.getElementById('containerList').innerHTML +=
 
                     `<div class="card" "` + media.title + `" style="width: 18rem;">` +
                     `<img class="card-img-top" src="` + media.img + `" alt="Card image cap">` +
@@ -120,7 +101,7 @@ document.getElementById('btnAddMedia').addEventListener('click', function () {
             }
 
         } else {
-            document.getElementById('list').innerHTML +=
+            document.getElementById('containerList').innerHTML +=
 
                 `<div class="card" "` + media.title + `" style="width: 18rem;">` +
                 `<img class="card-img-top" src="` + media.img + `" alt="Card image cap">` +
@@ -142,64 +123,135 @@ document.getElementById('btnAddMedia').addEventListener('click', function () {
 
 /////////////////////////////////////////////////////////////LocalStorageAffichage
 
-let data = localStorage.getItem("Collection");
-console.log(localStorage.getItem("Collection"));
-
-let dataParse = JSON.parse(data);
-let txt = "";
-
-dataParse.forEach((e) => {
-    txt +=
-        `<div class="card" "` + e.title + `" style="width: 18rem;">` +
-        `<img class="card-img-top" src="` + e.img + `" alt="Card image cap">` +
-        '<div class="card-body">' +
-        '<h5 class="card-title" id="titleOfMedia">' + e.title + '</h5>' +
-        '<p class="card-text">' + e.releaseDate + '</p>' +
-        '<p class="card-text">' + e.descritpion + '</p>' +
-        '</div>' + `<button type="button" id="remove` + i + `" class="btn-delete">delete</button>` +
-        `<button type="button" id="edit` + i + `" class="btn btn-primary">edit</button>` +
-        '</div>';
-});
-console.log(txt);
-document.getElementById("list").innerHTML = txt;
+affichage("All");
 
 
-document.getElementById('trie').addEventListener('click', function () {
-    console.log("trie");
-    const card = document.querySelectorAll('.card');
-
-    const cardArray = Array.from(card);
-    if (document.getElementById('trie').value == "nom") {
-        console.log("trie par nom");
-        let cardSortByName = cardArray.sort((a, b) => {
-            const nameA = a.getAttribute("name");
-            const nameB = b.getAttribute("name");
-            console.log(nameA);
-            if (nameA < nameB) {
-                return -1;
-            }
-            if (nameA > nameB) {
-                return 1;
-            }
-
-            // names must be equal
-            return 0;
-        });
-
-        let containerList = document.getElementById('containerList');
-
-        deleteChild(containerList);
+function affichage(type) {
+    let data = localStorage.getItem("Collection");
+    if (data !== null) {
+        let dataParse = JSON.parse(data);
+        let txt = "";
 
 
-        for (i = 0; i < cardSortByName.length; i++) {
-            console.log(cardSortByName[i].getAttribute("name"));
-            containerList.append(cardSortByName[i]);
+        if (type === "All") {
+            dataParse.forEach((e) => {
+                txt +=
+                    `<div class="card" "` + e.title + `" style="width: 18rem;">` +
+                    `<img class="card-img-top" src="` + e.img + `" alt="Card image cap">` +
+                    '<div class="card-body">' +
+                    '<h5 class="card-title" id="titleOfMedia">' + e.title + '</h5>' +
+                    '<p class="card-text">' + e.releaseDate + '</p>' +
+                    '<p class="card-text">' + e.descritpion + '</p>' +
+                    '</div>' + `<button type="button" id="remove` + i + `" class="btn-delete">delete</button>` +
+                    `<button type="button" id="edit` + i + `" class="btn btn-primary">edit</button>` +
+                    '</div>';
+            });
         }
 
+
+        if (type === "Album") {
+            dataParse.forEach((e) => {
+                if (e.type === "Album-btn") {
+                    txt +=
+                        `<div class="card" "` + e.title + `" style="width: 18rem;">` +
+                        `<img class="card-img-top" src="` + e.img + `" alt="Card image cap">` +
+                        '<div class="card-body">' +
+                        '<h5 class="card-title" id="titleOfMedia">' + e.title + '</h5>' +
+                        '<p class="card-text">' + e.releaseDate + '</p>' +
+                        '<p class="card-text">' + e.descritpion + '</p>' +
+                        '</div>' + `<button type="button" id="remove` + i + `" class="btn-delete">delete</button>` +
+                        `<button type="button" id="edit` + i + `" class="btn btn-primary">edit</button>` +
+                        '</div>';
+                }
+            });
+
+
+        }
+
+        if (type === "Game") {
+            dataParse.forEach((e) => {
+                if (e.type === "Game-btn") {
+                    txt +=
+                        `<div class="card" "` + e.title + `" style="width: 18rem;">` +
+                        `<img class="card-img-top" src="` + e.img + `" alt="Card image cap">` +
+                        '<div class="card-body">' +
+                        '<h5 class="card-title" id="titleOfMedia">' + e.title + '</h5>' +
+                        '<p class="card-text">' + e.releaseDate + '</p>' +
+                        '<p class="card-text">' + e.descritpion + '</p>' +
+                        '</div>' + `<button type="button" id="remove` + i + `" class="btn-delete">delete</button>` +
+                        `<button type="button" id="edit` + i + `" class="btn btn-primary">edit</button>` +
+                        '</div>';
+                }
+            });
+        }
+
+        if (type === "Movie") {
+
+            dataParse.forEach((e) => {
+                console.log(e.type)
+                if (e.type === "Movie-btn") {
+                    txt +=
+                        `<div class="card" "` + e.title + `" style="width: 18rem;">` +
+                        `<img class="card-img-top" src="` + e.img + `" alt="Card image cap">` +
+                        '<div class="card-body">' +
+                        '<h5 class="card-title" id="titleOfMedia">' + e.title + '</h5>' +
+                        '<p class="card-text">' + e.releaseDate + '</p>' +
+                        '<p class="card-text">' + e.descritpion + '</p>' +
+                        '</div>' + `<button type="button" id="remove` + i + `" class="btn-delete">delete</button>` +
+                        `<button type="button" id="edit` + i + `" class="btn btn-primary">edit</button>` +
+                        '</div>';
+                }
+            });
+
+
+        }
+
+        //console.log(txt);
+        document.getElementById("list").innerHTML = txt;
     }
 
 
-});
+};
+
+function trie() {
+    console.log(document.getElementById('trie').value);
+    if (document.getElementById('trie').value !== "pas_trie") {
+        console.log("trie");
+        let data = localStorage.getItem("Collection");
+        let dataParse = JSON.parse(data);
+        let txt = "";
+        if (document.getElementById('trie').value == "nom") {
+
+            console.log("trie par nom");
+            let dataTrie = dataParse.sort((a, b) => {
+                return a.title.localeCompare(b.title);
+            });
+
+            dataTrie.forEach((e) => {
+                txt +=
+                    `<div class="card" "` + e.title + `" style="width: 18rem;">` +
+                    `<img class="card-img-top" src="` + e.img + `" alt="Card image cap">` +
+                    '<div class="card-body">' +
+                    '<h5 class="card-title" id="titleOfMedia">' + e.title + '</h5>' +
+                    '<p class="card-text">' + e.releaseDate + '</p>' +
+                    '<p class="card-text">' + e.descritpion + '</p>' +
+                    '</div>' + `<button type="button" id="remove` + i + `" class="btn-delete">delete</button>` +
+                    `<button type="button" id="edit` + i + `" class="btn btn-primary">edit</button>` +
+                    '</div>';
+
+            });
+        }
+
+
+        document.getElementById("list").innerHTML = txt;
+    } else {
+        affichage("All");
+    }
+}
+
+document.getElementById('trie').addEventListener('click', trie);
+
+window.addEventListener('load', trie);
 
 
 function deleteChild(element) {
